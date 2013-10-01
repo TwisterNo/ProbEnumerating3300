ebbe7773a07182d3b8253a18dae1a7ca8d25c34b
gyrkir12 <gyrkir12@nith.no>
2013-10-01 13:26:27 +0200

    Opprettet UnitTest for OrderLine, og laget positiv og negativ test for Id.

---
+++ Oppgave3/UnitTestProject1/OrderLineTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniStore.BusinessCore;
+using MiniStore.Interfaces;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class OrderLineTest
+    {
+        OrderLine orderLine = new OrderLine(20);
+
+        [TestMethod]
+        public void Test_AreEqual_Id()
+        {
+            int expectedId = 10;
+
+            orderLine.Id = 10;
+
+            Assert.AreEqual(expectedId, orderLine.Id);
+        }
+
+        [TestMethod]
+        public void Test_AreNotEqual_Id()
+        {
+            int expectedId = 20;
+
+            orderLine.Id = 10;
+
+            Assert.AreNotEqual(expectedId, orderLine.Id);
+        }
+    }
+}
